137a246e3f53897b47eaed849da3932318533b36
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-15 15:28:31 +0200

    final commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from requests import get
 from pprint import PrettyPrinter
 
-BASE_URL = 'https://data.nba.net'
-ALL_JSON = '/prod/v1/today.json'
+BASE_URL = "https://data.nba.net"
+ALL_JSON = "/prod/v1/today.json"
 
 printer = PrettyPrinter()
 
@@ -23,7 +23,7 @@ def get_scoreboard():
         clock = game['clock']
         period = game['period']
 
-        print('----------------------------------------------------')
+        print("------------------------------------------")
         print(f"{home_team['triCode']} vs {away_team['triCode']}")
         print(f"{home_team['score']} - {away_team['score']}")
         print(f"{clock} - {period['current']}")
@@ -31,9 +31,17 @@ def get_scoreboard():
 
 def get_stats():
     stats = get_links()['leagueTeamStatsLeaders']
-    data = get(BASE_URL + stats).json()
+    teams = get(
+        BASE_URL + stats).json()['league']['standard']['regularSeason']['teams']
 
-    printer.pprint(data.keys)
+    teams = list(filter(lambda x: x['name'] != "Team", teams))
+    teams.sort(key=lambda x: int(x['ppg']['rank']))
+
+    for i, team in enumerate(teams):
+        name = team['name']
+        nickname = team['nickname']
+        ppg = team['ppg']['avg']
+        print(f"{i + 1}. {name} - {nickname} - {ppg}")
 
 
 get_stats()
